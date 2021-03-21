6bc348e903b87ebb1c55f5bc3995a6f7ea038f2b
tano-taka <tanowaki.takamasa@gmail.com>
2021-03-21 21:12:14 +0900

    rubocop修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,13 +3,13 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    email                  {Faker::Internet.free_email}
-    password               {Faker::Internet.password(min_length: 6)}
-    password_confirmation  {password}
-    nickname               {Faker::Name.last_name}
-    first_name             {Faker::Japanese::Name.first_name}
-    last_name              {Faker::Japanese::Name.last_name}
-    birthday               {Faker::Date.backward}
+    email                  { Faker::Internet.free_email }
+    password               { Faker::Internet.password(min_length: 6) }
+    password_confirmation  { password }
+    nickname               { Faker::Name.last_name }
+    first_name             { Faker::Japanese::Name.first_name }
+    last_name              { Faker::Japanese::Name.last_name }
+    birthday               { Faker::Date.backward }
     first_name_kana        { person.first.katakana }
     last_name_kana         { person.last.katakana }
   end
