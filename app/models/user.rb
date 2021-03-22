81500505b7f9adf292cd5ea7b3f45533ae259f25
tano-taka <tanowaki.takamasa@gmail.com>
2021-03-22 12:18:26 +0900

    ユーザー管理機能（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'Full-width characters' } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]/, message: 'Full-width characters' } do
       validates :last_name 
       validates :first_name
     end
