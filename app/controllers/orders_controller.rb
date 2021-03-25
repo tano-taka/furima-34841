7219ba9fef1c47894d9ca72e7ac658e6e869046a
tano-taka <tanowaki.takamasa@gmail.com>
2021-03-25 20:36:36 +0900

    購入画面の商品情報の表記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
 
   def index
     @order_address = OrderAddress.new 
@@ -19,6 +20,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building_name, :phone_number ).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
