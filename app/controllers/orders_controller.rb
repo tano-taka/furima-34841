8f42040cb3e8b3b28f5b4367531a7e5bbd21319e
tano-taka <tanowaki.takamasa@gmail.com>
2021-03-25 15:05:23 +0900

    入力フォームの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @address = Address.new
+  end
+
+  def create
+    @order = Order.create(order_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture_id, :city, :addresses, :building_name, :phone_number).merge(order_id: @order.id)
   end
 end
