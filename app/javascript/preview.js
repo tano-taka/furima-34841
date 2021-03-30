bf39da6166c2e246145b867ae584ddd2bdb3600e
tano-taka <tanowaki.takamasa@gmail.com>
2021-03-30 18:55:01 +0900

    画像プレビュー機能実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,25 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+
+    const createImageHTML = (blob) => {
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', 'item_preview');
+      ImageList.appendChild(blobImage);
+    };
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+  
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  });
+}
