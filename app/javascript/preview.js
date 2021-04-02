040658b0639cdbf0ee58580069c77e7324308b49
tano-taka <tanowaki.takamasa@gmail.com>
2021-04-02 14:38:10 +0900

    リセットボタン作成中止

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -24,18 +24,24 @@ if (document.URL.match( /items/ )) {
       imageElement.appendChild(inputHTML)
       ImageList.appendChild(imageElement)
   
-      // 最上のinputがあったら隠してリセットボタン作成
-      const clickElement = document.querySelector('.click-upload')
-      if(clickElement) {
-        const removeElement = document.createElement('input')
-        removeElement.setAttribute('type', 'button')
-        removeElement.setAttribute('value', ' 画像リセット ')
-        removeElement.setAttribute('id', 'remove_image')
+      //inputを隠して
+      const clickElement = document.querySelector('.add_input')
+      clickElement.setAttribute('class', 'hidden add_input')
+      document.querySelector('p').innerHTML = "※画像を変更する際はリロードして初めからやり直してください※"
 
-        const imgUpload = document.querySelector('.img-upload')
-        imgUpload.appendChild(removeElement)
-        clickElement.setAttribute('class', 'hidden add_input')
-      }
+      
+      // // リセットボタン作成
+      // removeImage = document.querySelector('#remove_image')
+      // if (!removeImage) {
+      //   const removeElement = document.createElement('input')
+      //   removeElement.setAttribute('type', 'button')
+      //   removeElement.setAttribute('value', ' 画像リセット ')
+      //   removeElement.setAttribute('id', 'remove_image')
+      //   const imgUpload = document.querySelector('.img-upload')
+      //   imgUpload.appendChild(removeElement)
+      // }
+
+      
      
       inputHTML.addEventListener('change', (e) => {
         file = e.target.files[0];
@@ -48,40 +54,13 @@ if (document.URL.match( /items/ )) {
       })
     }
    
-    document.getElementById('item-image').addEventListener('change', function(e){
+    document.querySelector('.add_input').addEventListener('change', function(e){
       let file = e.target.files[0];
       let blob = window.URL.createObjectURL(file);
       createImageHTML(blob);
       
     });
      // 画像リセットボタン押すと画像が全て消える
-    document.getElementById('item-image').addEventListener('change', function(e){
-      const removeButton = document.getElementById('remove_image')
-      removeButton.addEventListener('click', function(){
-        const allForm = document.querySelectorAll('.hidden');
-        const allImages = document.querySelectorAll('img');
-        allImages.forEach(function( image ) {
-          image.remove() 
-        })
-        allForm.forEach(function( form ) {
-          form.remove() 
-        })
-        
-        const addInputElement = document.querySelector('.add_input')
-        addInputElement.remove()
-        removeButton.remove()
-        // inputボタン再設置
-
-        const imgUpload = document.querySelector('.img-upload')
-        const inputHTML = document.createElement('input')
-        let imageElementNum = document.querySelectorAll('.image-element').length
-        inputHTML.setAttribute('name', 'item[images][]')
-        inputHTML.setAttribute('type', 'file')
-        inputHTML.setAttribute('id', `item_image_${imageElementNum}`)
-        inputHTML.setAttribute('class', 'add_input')
-
-        imgUpload.appendChild(inputHTML)
-      })
-    })
   });
+
 }
